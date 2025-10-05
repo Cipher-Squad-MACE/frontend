e7712abdaedeedc0efd9dab2c5c336141adcdbcd
theja-anil <thejaanil15@gmail.com>
2025-10-05 18:18:39 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,7 @@
 import "../styles/globals.css";
+if (typeof window !== "undefined") {
+  window.addEventListener("error", e => console.log("React Error", e.message));
+}
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
